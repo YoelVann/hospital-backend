76b946da2c7d56adbdbfb1d843de027a7b7b5647
YoelVann <yoelvann@gmail.com>
2021-05-16 15:42:48 -0500

    Fin sección 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 
 const login = async(req, res = response) => {
@@ -36,8 +37,9 @@ const login = async(req, res = response) => {
 
         res.json({
             ok: true,
-            token
-        })
+            token,
+            menu: getMenuFrontEnd(usuarioDB.role)
+        });
 
 
     } catch (error) {
@@ -87,7 +89,8 @@ const googleSignIn = async(req, res = response) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd(usuario.role)
         });
 
     } catch (error) {
@@ -114,7 +117,8 @@ const renewToken = async(req, res = response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd(usuario.role)
     });
 }
 
--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Usuario = require('../models/usuario')
 
 const validarJWT = (req, res, next) => {
     // leer token
@@ -25,8 +26,77 @@ const validarJWT = (req, res, next) => {
     }
 }
 
+const validarADMIN_ROLE = async(req, res, next) => {
 
+    const uid = req.uid;
+
+    try {
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'usuario no existe'
+            });
+        }
+
+        if (usuarioDB.role !== 'ADMIN_ROLE') {
+            return res.status(403).json({
+                ok: false,
+                msg: 'No tiene provilegios para hacer eso'
+            });
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'algo salió mal, hable con el admin'
+        });
+    }
+
+}
+
+const validarADMIN_ROLEoMismoUsuario = async(req, res, next) => {
+
+    const uid = req.uid;
+    const id = req.params.id;
+
+    try {
+        const usuarioDB = await Usuario.findById(uid);
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'usuario no existe'
+            });
+        }
+
+        if (usuarioDB.role === 'ADMIN_ROLE' || uid === id) {
+            next();
+        } else {
+            return res.status(403).json({
+                ok: false,
+                msg: 'No tiene provilegios para hacer eso'
+            });
+
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'algo salió mal, hable con el admin'
+        });
+    }
+
+}
 
 module.exports = {
     validarJWT,
+    validarADMIN_ROLE,
+    validarADMIN_ROLEoMismoUsuario
 }
